824821faaf1679a2495fb49d6bfdb898788c187b
Matthew Wilding <mbwilding@gmail.com>
2024-04-07 16:50:37 +0800

    Update rust.lua

--- lua/languages/rust.lua
+++ lua/languages/rust.lua
@@ -1,37 +1,72 @@
 return {
-	Run = {
+	RunDebug = {
 		terminal_commands = {
 			[1] = {
 				"cargo run",
-			}
-		}
+			},
+		},
 	},
-	Build = {
+	RunRelease = {
+		terminal_commands = {
+			[1] = {
+				"cargo run --release",
+			},
+		},
+	},
+	BuildDebug = {
 		terminal_commands = {
 			[1] = {
 				"cargo build",
-			}
-		}
+			},
+		},
+	},
+	BuildRelease = {
+		terminal_commands = {
+			[1] = {
+				"cargo build --release",
+			},
+		},
 	},
 	Update = {
 		terminal_commands = {
 			[1] = {
 				"cargo update",
-			}
-		}
+			},
+		},
 	},
 	Test = {
 		terminal_commands = {
 			[1] = {
 				"cargo test",
-			}
-		}
+			},
+		},
 	},
 	Clean = {
 		terminal_commands = {
 			[1] = {
 				"cargo clean",
-			}
-		}
-	}
+			},
+		},
+	},
+	Clippy = {
+		terminal_commands = {
+			[1] = {
+				"cargo clippy",
+			},
+		},
+	},
+	Check = {
+		terminal_commands = {
+			[1] = {
+				"cargo check",
+			},
+		},
+	},
+	Format = {
+		terminal_commands = {
+			[1] = {
+				"cargo fmt",
+			},
+		},
+	},
 }
